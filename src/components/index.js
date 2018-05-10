f0f8924f75ba455e6ade3fb8e8438505992ed2d6
Chao Zhang <chao.zhang@workday.com>
2018-05-10 01:45:33 -0700

    add components lib

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Header from './components/header'
+import Icon from './components/icons'
+import Nav from './components/nav'
+import Footer from './components/footer'
+
+
+module.exports = {
+  Header,
+  Icon,
+  Nav,
+  Footer
+};
