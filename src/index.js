a713174c03172c9db619c12423a87ef98cb69826
Chao Zhang <chao.zhang@workday.com>
2018-05-09 23:56:23 -0700

    add normalize css

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/';
 import registerServiceWorker from './registerServiceWorker';
-import appCss from './index.css';
+
+// load styles
+import 'normalize.css';
+import './index.css';
 
 
+// render react page
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
