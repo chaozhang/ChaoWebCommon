4317ee8b1b568c9ba8754bdfac9e83d378e6880e
Chao Zhang <chao.zhang@workday.com>
2018-05-09 22:21:49 -0700

    change icons path

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 const Icon = (props) => {
     const htmlString = {
-        __html: `<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="${props.assetPath}/assets/sprite.svg#${props.id}"></use>`
+        __html: `<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="${props.assetPath || ''}/assets/sprite.svg#${props.id}"></use>`
     };
 
     return (
@@ -15,7 +15,7 @@ const Icon = (props) => {
 
 Icon.propTypes = {
     id: PropTypes.string.isRequired,
-    assetPath: PropTypes.string.isRequired
+    assetPath: PropTypes.string
 }
 
 
